08684e6ded0bd92d145110bdcb4844a5efd54392
Jordi Sabanés <43608629+JordiiSM@users.noreply.github.com>
2026-04-09 16:38:03 +0200

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const mobs = [
+  {
+    name:"Orco Negro",
+    level:42,
+    drops:[
+      {name:"Espada Luna Llena", chance:"12%"},
+      {name:"Armadura de Placas", chance:"8%"},
+      {name:"Poción Roja (M)", chance:"45%"}
+    ]
+  },
+  {
+    name:"Metin de la Sombra",
+    level:50,
+    drops:[
+      {name:"Libro de habilidad", chance:"20%"},
+      {name:"Piedra espiritual", chance:"5%"},
+      {name:"Yang", chance:"80%"}
+    ]
+  },
+  {
+    name:"Capitán Araña",
+    level:60,
+    drops:[
+      {name:"Collar de Oro", chance:"10%"},
+      {name:"Armadura +6", chance:"6%"},
+      {name:"Cofre araña", chance:"15%"}
+    ]
+  },
+  {
+    name:"Rey Demonio",
+    level:75,
+    drops:[
+      {name:"Espada +9", chance:"2%"},
+      {name:"Pergamino Bendición", chance:"18%"},
+      {name:"Cofre demonio", chance:"12%"}
+    ]
+  }
+];
